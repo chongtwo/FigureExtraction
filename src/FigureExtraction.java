7cafd7233c96d09514a875bfcdea1e1e5a0c383b
Wang Yuhong <358397459@qq.com>
2018-03-14 08:58:14 +0800

    Add XlsOperator; Achieve Dictionary importing

--- src/FigureExtraction.java
+++ src/FigureExtraction.java
@@ -6,6 +6,7 @@ public class FigureExtraction
         ShortSentence.intializeDictionary("C:\\Users\\W\\Desktop\\人工词典积累.xls");
         LongSentence longSentence = new LongSentence("胸廓对称。左上叶有小结节，右肺结节；");
         longSentence.segToShort();
+        longSentence.shortSentences.get(0).match();
 //        System.out.println(longSentence.numOfShortSentences);
 //        for (ShortSentence ss : longSentence.shortSentences){
 //            System.out.println(ss.content);
--- src/ShortSentence.java
+++ src/ShortSentence.java
@@ -1,9 +1,17 @@
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ShortSentence {
 
+    static String rule;
+    private String content;
+    String semanticSentence;
+    String primaryLocation = "";
+    String descriptor = "";
+    String figure = "";
     static HashMap<String, String> semanticDictionary;
 
     /**
@@ -13,23 +21,25 @@ public class ShortSentence {
     static void intializeDictionary(String path){
         XlsOperator xlsOperator = new XlsOperator();
         semanticDictionary = xlsOperator.readXls(path);
-        for(Map.Entry<String, String> entry : semanticDictionary.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+        //打印semanticDictionary
+//        for(Map.Entry<String, String> entry : semanticDictionary.entrySet()) {
+//            System.out.println(entry.getKey() + " : " + entry.getValue());
+//        }
     }
-
-    static String rule;
-
-    String content;
-    String primaryLocation;
-    String figure;
-
 
     public void setContent(String value){
         content = value;
+        semanticSentence = content;
     }
 
     public void match(){
-
+        int numOfMatched = 0;
+        for(Map.Entry<String, String> entry : semanticDictionary.entrySet()){
+            if (semanticSentence.contains(entry.getKey())) {
+                semanticSentence = semanticSentence.replace(entry.getKey(), entry.getValue() + String.valueOf(numOfMatched));
+                numOfMatched++;
+            }
+        }
+        System.out.println(semanticSentence);
     }
 }
