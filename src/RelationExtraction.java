3d9a9af0fb0899139d1590b8c58136eab868d615
Wang Yuhong <358397459@qq.com>
2018-03-14 16:51:13 +0800

    RelationExtraction

--- src/RelationExtraction.java
+++ src/RelationExtraction.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class RelationExtraction {
 //    public String lastPrimaryLocation = "";
     ArrayList<String> PrimaryLocation = new ArrayList<String>();
+    HashMap<String,String> map = new HashMap<String, String>();
+    Pattern P = Pattern.compile("(主干部位#[0-9]+#)|(区域#[0-9]+#)(细节部位#[0-9]+#)(主干部位#[0-9]+#)");
 
 }
