1e7c310fae60b030cfedad3e8d75b9e46a2cb7e8
Wang Yuhong <358397459@qq.com>
2018-05-03 10:37:43 +0800

    RMM

--- src/RelationExtraction.java
+++ src/RelationExtraction.java
@@ -1,6 +1,7 @@
 import com.sun.org.apache.xpath.internal.operations.Variable;
 
 import javax.lang.model.element.VariableElement;
+import java.awt.*;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,10 +12,20 @@ import java.util.regex.Pattern;
 public class RelationExtraction {
 
     static String lastPrimaryLocation = "";
-    static Pattern P = Pattern.compile("(PrimaryLocation#[0-9]+#)|(Region#[0-9]+#)(SpecificLocation#[0-9]+#)(PrimaryLocation#[0-9]+#)");
+    static ArrayList<Pattern> patternList = new ArrayList<Pattern>();
 
+    private ArrayList<Pattern> compilePattern(){
+        Pattern p = Pattern.compile("(PrimaryLocation#[0-9]+#)|(Region#[0-9]+#)(SpecificLocation#[0-9]+#)?(PrimaryLocation#[0-9]+#)");//气管、左右主支气管及其分支开口未见狭窄、中断。
+        Pattern p2 = Pattern.compile("(#Descriptor#[0-9]+#)(Change#[0-9]+#)(DiagnosisSuffix#[0-9]+#)|(Descriptor#[0-9]+#)(Descriptor#[0-9]+#)(DiagnosisSuffix#[0-9]+#)");//左肺上叶尖后段另见条索状密度增高影及点状高密度影
+        Pattern p3 = Pattern.compile("PrimaryLocation#[0-9]+#SpecificLocation#[0-9]+#Possibility#[0-9]+#Diagnosis#[0-9]+#DescriptorSuffix#[0-9]+#、Descriptor#[0-9]+#" +
+                "及Descriptor#[0-9]+#Descriptor#[0-9]+#Change#[0-9]+#DiagnosisSuffix#[0-9]+#及Quantifier#[0-9]+#Diagnosis#[0-9]+#DescriptorSuffix#[0-9]+#Descriptor#[0-9]+#DiagnosisSuffix#[0-9]+#，");//右肺上叶尖段见结节状、条索状及絮状密度增高影及一结节状高密度影，
+        patternList.add(p);
+        patternList.add(p2);
+        patternList.add(p3);
+        return  patternList;
+    }
 
-    public static HashMap<Integer, StructuredShortSentence> relationExtract(String semanticSentence, HashMap<String, String> matchedDictionary){
+    public HashMap<Integer, StructuredShortSentence> relationExtract(String semanticSentence, HashMap<String, String> matchedDictionary){
         String primaryLocation = "";
         String specificLocation = "";
         String region = "";
@@ -26,11 +37,13 @@ public class RelationExtraction {
         String measureLocation = "";
         String value= "";
         String unit = "";
-        Matcher m = P.matcher(semanticSentence);
+        ArrayList<Pattern> patternArrayList = compilePattern();
+        Matcher m = patternArrayList.get(1).matcher(semanticSentence);
         int numOfFind = 0;
         HashMap<Integer, StructuredShortSentence> numMap = new HashMap<>();//用于记录有几个find
         numMap.put(numOfFind, new StructuredShortSentence());//先初始化，避免m.find为空时(句子缺少主干部位时)的空指针，如果m.find不为空，numMap.put将会覆盖该条
         numMap.get(numOfFind).primaryLocation = lastPrimaryLocation;//primaryLocation默认是上次的，如果有新的，则在while(m.find())中更新
+        
         while(m.find()){
             primaryLocation = lastPrimaryLocation;//primaryLocation默认是上次的，如果有新的，则在while(m.find())中更新
             specificLocation = "";
